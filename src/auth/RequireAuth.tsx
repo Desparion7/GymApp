165229ef4c4cc2bb5a6dbddecab2fed4020089c0
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-02-14 22:06:30 +0100

    block acces to routes by RequireAuth

---
+++ src/auth/RequireAuth.tsx
@@ -0,0 +1,21 @@
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import useAuthToken from '../hooks/useAuthToken';
+
+const RequireAuth = () => {
+	const location = useLocation();
+	const { username } = useAuthToken();
+	return (
+		<>
+			{username ? (
+				<Outlet />
+			) : (
+				<Navigate
+					to='/'
+					state={{ from: location }}
+					replace
+				/>
+			)}
+		</>
+	);
+};
+export default RequireAuth
