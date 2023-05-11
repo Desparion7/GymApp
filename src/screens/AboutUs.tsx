4869796fad6b3ff5fa7f1321b4426a46b651b30a
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-05-11 12:35:45 +0200

    add simple animation for img

--- src/screens/AboutUs.tsx
+++ src/screens/AboutUs.tsx
@@ -8,29 +8,31 @@ const AboutUs = () => {
 				<div className='aboutUs__section-text'>
 					<h3>Czy jest Menadżer treningu?</h3>
 					<p>
-						Menadżer treningu to aplikacja stworzona specjalnie dla osób, które
-						dbają o swoją formę i zdrowie. Dzięki niej można z łatwością
-						zapisywać historię swoich treningów, zarządzać planami treningowymi
-						oraz korzystać z opisów ćwiczeń.
+						Menadżer treningu to aplikacja stworzona specjalnie dla
+						osób, które dbają o swoją formę i zdrowie. Dzięki niej
+						można z łatwością zapisywać historię swoich treningów,
+						zarządzać planami treningowymi oraz korzystać z opisów
+						ćwiczeń.
 					</p>
 				</div>
-				<div className='aboutUs-img'>
+				<div className='aboutUs__img from_right'>
 					<img src='../../img/weights.PNG' alt='Ciężarki' />
 				</div>
 			</div>
 
 			<div className='aboutUs__section'>
-				<div className='aboutUs-img middle'>
+				<div className='aboutUs__img middle from_left'>
 					<img src='../../img/notes.PNG' alt='Ciężarki' />
 				</div>
 				<div className='aboutUs__section-text '>
 					<h3>Najwazniejsza Funkcja?</h3>
 					<p>
-						Ważnym elementem menadżera treningu jest możliwość zapisywania
-						historii treningów. Dzięki temu możesz śledzić swoje postępy,
-						monitorować swoje osiągnięcia oraz dostosowywać swoje plany
-						treningowe do swoich aktualnych możliwości. Dzięki temu będziesz
-						mógł zachować motywację do dalszej pracy nad swoim ciałem i
+						Ważnym elementem menadżera treningu jest możliwość
+						zapisywania historii treningów. Dzięki temu możesz
+						śledzić swoje postępy, monitorować swoje osiągnięcia
+						oraz dostosowywać swoje plany treningowe do swoich
+						aktualnych możliwości. Dzięki temu będziesz mógł
+						zachować motywację do dalszej pracy nad swoim ciałem i
 						zdrowiem.
 					</p>
 				</div>
@@ -39,16 +41,18 @@ const AboutUs = () => {
 				<div className='aboutUs__section-text'>
 					<h3>Plany indywidualne</h3>
 					<p>
-						Nasza aplikacja daje dostęp do przykładowych treningów. Jeśli jednak
-						potrzebujesz indywidualnego podejścia do treningu i diety, to z
-						pomocą przychodzą nasi specjaliści. Nasza aplikacja umożliwia
-						zakupienie spersonalizowanego planu treningowego i diety, które
-						zostaną przygotowane przez naszych doświadczonych trenerów oraz
-						dietetyków. Dzięki temu masz pewność, że Twoje plany są dopasowane
-						do Twoich indywidualnych potrzeb i celów.
+						Nasza aplikacja daje dostęp do przykładowych treningów.
+						Jeśli jednak potrzebujesz indywidualnego podejścia do
+						treningu i diety, to z pomocą przychodzą nasi
+						specjaliści. Nasza aplikacja umożliwia zakupienie
+						spersonalizowanego planu treningowego i diety, które
+						zostaną przygotowane przez naszych doświadczonych
+						trenerów oraz dietetyków. Dzięki temu masz pewność, że
+						Twoje plany są dopasowane do Twoich indywidualnych
+						potrzeb i celów.
 					</p>
 				</div>
-				<div className='aboutUs-img'>
+				<div className='aboutUs__img'>
 					<button
 						className='greenBtn'
 						onClick={() => {
