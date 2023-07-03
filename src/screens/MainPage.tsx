acdc30e24b06d432cabe592454ec5cee877a790e
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-07-03 09:29:25 +0200

    add img loading on main page

--- src/screens/MainPage.tsx
+++ src/screens/MainPage.tsx
@@ -1,37 +1,53 @@
 import '../css/MainPage.css';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const MainPage = () => {
 	const navigate = useNavigate();
+	const [imgLoaded, setImgLoaded] = useState(false);
+
+	useEffect(() => {
+		const img = new Image();
+		img.src = './img/bg.png';
+		img.onload = () => {
+			setImgLoaded(true);
+		};
+	});
+
 	return (
-		<section className='mainPage'>
-			<div className='mainPage__topSection'>
-				<div className='mainPage__topSection-textSection'>
-					<h3>
-						"Nie bądź zwykły, bądź najlepszy - doskonal swoją formę i zdrowie z
-						naszą aplikacją."
-					</h3>
-					<div className='mainPage__topSection-textSection-btnbox'>
-						<button
-							className='greenBtn'
-							onClick={() => {
-								navigate('/login');
-							}}
-						>
-							Zaloguj się
-						</button>
-						<button
-							className='greenBtn'
-							onClick={() => {
-								navigate('/about');
-							}}
-						>
-							Więcej
-						</button>
+		<>
+			{imgLoaded && (
+				<section className='mainPage'>
+					<img src='./img/bg.png' alt='dsaasd' />
+					<div className='mainPage__topSection'>
+						<div className='mainPage__topSection-textSection'>
+							<h3>
+								"Nie bądź zwykły, bądź najlepszy - doskonal
+								swoją formę i zdrowie z naszą aplikacją."
+							</h3>
+							<div className='mainPage__topSection-textSection-btnbox'>
+								<button
+									className='greenBtn'
+									onClick={() => {
+										navigate('/login');
+									}}
+								>
+									Zaloguj się
+								</button>
+								<button
+									className='greenBtn'
+									onClick={() => {
+										navigate('/about');
+									}}
+								>
+									Więcej
+								</button>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-		</section>
+				</section>
+			)}
+		</>
 	);
 };
 
